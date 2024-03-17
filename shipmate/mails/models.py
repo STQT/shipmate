61f3ba96633df06a3820afc2c00492549b375375
STQT <productgsm@gmail.com>
2024-03-17 22:32:26 +0500

    fix mail object name

--- shipmate/mails/models.py
+++ shipmate/mails/models.py
@@ -5,7 +5,7 @@ class Mail(models.Model):
     subject = models.CharField(max_length=100)
     sender = models.CharField(max_length=320)
     recipient = models.CharField(max_length=320)
-    date = models.CharField(20)
+    date = models.CharField(max_length=50)
     body = models.TextField()
 
     def __str__(self):

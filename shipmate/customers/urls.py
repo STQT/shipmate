5feb48ec75315737900de1d538480690498deced
STQT <productgsm@gmail.com>
2024-03-23 11:14:50 +0500

    customers crud

--- shipmate/customers/urls.py
+++ shipmate/customers/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from .views import ListCustomerAPIView
+from ..leads.views import (
+    CreateLeadsAPIView,
+    UpdateLeadsAPIView,
+    DetailLeadsAPIView,
+    DeleteLeadsAPIView
+)
 
 urlpatterns = [
     path('customer-list/', ListCustomerAPIView.as_view(), name='customer-list'),
+    path('create/', CreateLeadsAPIView.as_view(), name='customer-create'),
+    path('<int:pk>/update/', UpdateLeadsAPIView.as_view(), name='customer-update'),
+    path('<int:pk>/detail/', DetailLeadsAPIView.as_view(), name='customer-detail'),
+    path('<int:pk>/delete/', DeleteLeadsAPIView.as_view(), name='customer-delete'),
 ]
--- shipmate/customers/views.py
+++ shipmate/customers/views.py
@@ -2,9 +2,29 @@ from rest_framework.generics import ListAPIView
 
 from .models import Customer
 from .serializers import CustomerSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, CreateAPIView, UpdateAPIView
 
 
 class ListCustomerAPIView(ListAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
+
+class CreateCustomerAPIView(CreateAPIView):  # noqa
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class UpdateCustomerAPIView(UpdateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class DeleteCustomerAPIView(DestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class DetailCustomerAPIView(RetrieveAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer

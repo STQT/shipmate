9a75635d268792077a3c1abf671476620ceab92d
STQT <productgsm@gmail.com>
2024-03-24 20:50:46 +0500

    add car marks filter and model filter

---
+++ shipmate/cars/filters.py
@@ -0,0 +1,30 @@
+import django_filters
+from .models import CarMarks, CarsModel
+
+
+class CarMarksFilter(django_filters.FilterSet):
+    q = django_filters.CharFilter(method='custom_filter')
+
+
+    class Meta:
+        model = CarMarks
+        fields = ['q']
+
+    def custom_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(name__icontains=value)
+        return queryset
+
+
+class CarsModelFilter(django_filters.FilterSet):
+    q = django_filters.CharFilter(method='custom_filter')
+    mark = django_filters.CharFilter(field_name='mark__id')
+
+    class Meta:
+        model = CarsModel
+        fields = ['q', 'mark']
+
+    def custom_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(name__icontains=value)
+        return queryset
--- shipmate/cars/views.py
+++ shipmate/cars/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 
+from .filters import CarMarksFilter, CarsModelFilter
 from .models import CarsModel, CarMarks
 from .serializers import CarsModelSerializer, CarMarksSerializer
 
@@ -7,9 +8,10 @@ from .serializers import CarsModelSerializer, CarMarksSerializer
 class ListCarsModelAPIView(ListAPIView):
     queryset = CarsModel.objects.all()
     serializer_class = CarsModelSerializer
-    filterset_fields = ["mark"]
+    filterset_class = CarsModelFilter
 
 
 class ListCarMarksAPIView(ListAPIView):
     queryset = CarMarks.objects.all()
     serializer_class = CarMarksSerializer
+    filterset_class = CarMarksFilter

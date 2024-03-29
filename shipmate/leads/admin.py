9bf97a23c98b8b14db4fef7569bdbbaac1b63bfb
STQT <productgsm@gmail.com>
2024-03-29 23:49:23 +0500

    admin panel adding

--- shipmate/leads/admin.py
+++ shipmate/leads/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from shipmate.leads.models import Leads, LeadsAttachment
+from shipmate.leads.models import Leads, LeadsAttachment, LeadVehicles
 
 
 @admin.register(Leads)
 class LeadsAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["origin", "destination", "customer",]
+    autocomplete_fields = ["origin", "destination", "customer", ]
     ordering = ["-id"]
     list_display = ["customer", "status", "price"]
 
@@ -13,3 +13,9 @@ class LeadsAdmin(admin.ModelAdmin):
 @admin.register(LeadsAttachment)
 class LeadsAttachmentAdmin(admin.ModelAdmin):
     ...
+
+
+@admin.register(LeadVehicles)
+class LeadVehiclesAdmin(admin.ModelAdmin):
+    list_display = ['vehicle', 'vehicle_year', 'lead']
+    autocomplete_fields = ['vehicle']

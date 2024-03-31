01ff3b851afd0e236a078fa34c6c94dc6c8e0477
STQT <productgsm@gmail.com>
2024-03-31 17:59:57 +0500

    fix create cars model and marks

--- shipmate/cars/urls.py
+++ shipmate/cars/urls.py
@@ -11,8 +11,8 @@ from .views import (
 urlpatterns = [
     path('marks-list/', ListCarMarksAPIView.as_view(), name='car-marks-list'),
     path('marks/<int:pk>/', UpdateRetrieveDestroyCarMarksAPIView.as_view(), name='car-marks-rud'),
-    path('marks/<int:pk>/create/', CreateCarMarksAPIView.as_view(), name='car-marks-create'),
+    path('marks/create/', CreateCarMarksAPIView.as_view(), name='car-marks-create'),
     path('models-list/', ListCarsModelAPIView.as_view(), name='car-models-list'),
     path('models/<int:pk>/', UpdateRetrieveDestroyCarsModelAPIView.as_view(), name='car-models-rud'),
-    path('models/<int:pk>/create/', CreateCarsModelAPIView.as_view(), name='car-models-create'),
+    path('models/create/', CreateCarsModelAPIView.as_view(), name='car-models-create'),
 ]

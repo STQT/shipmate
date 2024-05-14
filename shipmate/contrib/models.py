6c5709d429fc1a66ff1fd26326b2e1328dd5b086
STQT <productgsm@gmail.com>
2024-05-14 11:36:41 +0500

    fix quotes

--- shipmate/contrib/models.py
+++ shipmate/contrib/models.py
@@ -79,20 +79,20 @@ class LeadsAbstract(models.Model):
 class QuoteAbstract(LeadsAbstract):
     status = models.CharField(max_length=20, choices=QuoteStatusChoices.choices, default=QuoteStatusChoices.QUOTES)
     # Payments
-    payment_total_tariff = models.PositiveIntegerField(default=0)
-    payment_reservation = models.PositiveIntegerField(default=0)
-    payment_paid_reservation = models.PositiveIntegerField(default=0)
-    payment_carrier_pay = models.PositiveIntegerField(default=0)
-    payment_cod_to_carrier = models.PositiveIntegerField(default=0)
-    payment_paid_to_carrier = models.PositiveIntegerField(default=0)
+    # payment_total_tariff = models.PositiveIntegerField(default=0)
+    # payment_reservation = models.PositiveIntegerField(default=0)
+    # payment_paid_reservation = models.PositiveIntegerField(default=0)
+    # payment_carrier_pay = models.PositiveIntegerField(default=0)
+    # payment_cod_to_carrier = models.PositiveIntegerField(default=0)
+    # payment_paid_to_carrier = models.PositiveIntegerField(default=0)
 
     # Date
-
-    date_est_pu = models.DateField(null=True, blank=True)
-    date_est_del = models.DateField(null=True, blank=True)
-    date_dispatched = models.DateField(null=True, blank=True)
-    date_picked_up = models.DateField(null=True, blank=True)
-    date_delivered = models.DateField(null=True, blank=True)
+    #
+    # date_est_pu = models.DateField(null=True, blank=True)
+    # date_est_del = models.DateField(null=True, blank=True)
+    # date_dispatched = models.DateField(null=True, blank=True)
+    # date_picked_up = models.DateField(null=True, blank=True)
+    # date_delivered = models.DateField(null=True, blank=True)
 
     class Meta:
         default_related_name = 'quotes'

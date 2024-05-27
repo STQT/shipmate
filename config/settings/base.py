d44cf0b81db79311de6ada4ac7d9c1c452ef3262
STQT <productgsm@gmail.com>
2024-05-27 13:07:15 +0500

    fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -357,7 +357,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),  # Adjust as needed
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),  # Adjust as needed
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),  # Adjust as needed
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,

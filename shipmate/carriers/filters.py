a3f2b4958eb8c5ee17211fddf389a7b03b8154f0
STQT <productgsm@gmail.com>
2024-05-30 16:03:26 +0500

    carrier name

--- shipmate/carriers/filters.py
+++ shipmate/carriers/filters.py
@@ -5,7 +5,8 @@ from .models import Carrier
 
 class CarrierFilter(django_filters.FilterSet):
     status = django_filters.ChoiceFilter(choices=Carrier.CarrierStatus.choices)
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Carrier
-        fields = ['status']
+        fields = ['status', 'name']

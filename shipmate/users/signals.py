c17bea0a38d43310f53a31ac3ef07cafbedb3987
STQT <productgsm@gmail.com>
2024-05-30 20:36:01 +0500

    fix users signals

--- shipmate/users/signals.py
+++ shipmate/users/signals.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 
 from shipmate.contrib.logging import log_update, store_old_values
 from shipmate.contrib.models import UserLog
+from shipmate.users.models import Team, TeamLog, Role, RoleLog, FeatureLog, Feature
 
 User = get_user_model()
 
@@ -16,3 +17,33 @@ def log_store_user_data(sender, instance, **kwargs):
 @receiver(post_save, sender=User)
 def log_user_update(sender, instance, created, **kwargs):
     log_update(sender, instance, created, UserLog, "user", **kwargs)
+
+
+@receiver(pre_save, sender=Team)
+def log_store_user_data(sender, instance, **kwargs):
+    store_old_values(sender, instance, **kwargs)
+
+
+@receiver(post_save, sender=Team)
+def log_user_update(sender, instance, created, **kwargs):
+    log_update(sender, instance, created, TeamLog, "team", **kwargs)
+
+
+@receiver(pre_save, sender=Role)
+def log_store_user_data(sender, instance, **kwargs):
+    store_old_values(sender, instance, **kwargs)
+
+
+@receiver(post_save, sender=Role)
+def log_user_update(sender, instance, created, **kwargs):
+    log_update(sender, instance, created, RoleLog, "role", **kwargs)
+
+
+@receiver(pre_save, sender=Feature)
+def log_store_user_data(sender, instance, **kwargs):
+    store_old_values(sender, instance, **kwargs)
+
+
+@receiver(post_save, sender=Feature)
+def log_user_update(sender, instance, created, **kwargs):
+    log_update(sender, instance, created, FeatureLog, "feature", **kwargs)

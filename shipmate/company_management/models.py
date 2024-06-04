b86b7ae3127eeda8647f2a0219568869e0238526
STQT <productgsm@gmail.com>
2024-06-05 00:09:39 +0500

    fix

--- shipmate/company_management/models.py
+++ shipmate/company_management/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
+from shipmate.utils.models import BaseLog
+
 
 class CompanyInfo(models.Model):
     name = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
     mainline = models.CharField(max_length=20)
     fax = models.CharField(max_length=20)
     email = models.EmailField()
+    support_email = models.EmailField()
+    accounting_email = models.EmailField()
     address = models.TextField()
+    mon_fri = models.CharField(max_length=50)
+    saturday = models.CharField(max_length=50)
+    sunday = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+
+class CompanyInfoLog(BaseLog):
+    company_info = models.ForeignKey("CompanyInfo", on_delete=models.CASCADE, related_name="logs")
--- shipmate/company_management/serializers.py
+++ shipmate/company_management/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import CompanyInfo
+from .models import CompanyInfo, CompanyInfoLog
+
+
+class CompanyInfoLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyInfoLog
+        fields = ("title", "message")
 
 
 class CompanyInfoSerializer(serializers.ModelSerializer):
+    logs = CompanyInfoLogSerializer(many=True, read_only=True)
+
     class Meta:
         model = CompanyInfo
-        fields = ['name', 'mainline', 'fax', 'email', 'address']
+        fields = "__all__"

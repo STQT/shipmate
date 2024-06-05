2a52e7600ecd469555463af63a3f7433da7d037c
STQT <productgsm@gmail.com>
2024-06-05 19:05:59 +0500

    fix

--- shipmate/company_management/admin.py
+++ shipmate/company_management/admin.py
@@ -1,23 +1,48 @@
 from django.contrib import admin
 
-from shipmate.company_management.models import CompanyInfo, Merchant, VoIP, Template
+from shipmate.company_management.models import CompanyInfo, Merchant, VoIP, Template, CompanyInfoLog, MerchantLog, \
+    VoIPLog, TemplateLog
+
+
+class CompanyInfoLogInline(admin.TabularInline):
+    model = CompanyInfoLog
+    extra = 0
+
+
+class MerchantLogInline(admin.TabularInline):
+    model = MerchantLog
+    extra = 0
+
+
+class VoIPLogInline(admin.TabularInline):
+    model = VoIPLog
+    extra = 0
+
+
+class TemplateLogInline(admin.TabularInline):
+    model = TemplateLog
+    extra = 0
 
 
 @admin.register(CompanyInfo)
 class CompanyInfoAdmin(admin.ModelAdmin):
     list_display = ['name', 'mainline', 'fax', 'email', 'address']
+    inlines = [CompanyInfoLogInline]
 
 
 @admin.register(Merchant)
 class MerchantAdmin(admin.ModelAdmin):
     list_display = ['name', 'status', 'merchant_type']
+    inlines = [MerchantLogInline]
 
 
 @admin.register(VoIP)
 class VoIPAdmin(admin.ModelAdmin):
     list_display = ['name', 'status', 'voip_type']
+    inlines = [VoIPLogInline]
 
 
 @admin.register(Template)
 class TemplateAdmin(admin.ModelAdmin):
     list_display = ['name', 'status', 'template_type']
+    inlines = [TemplateLogInline]

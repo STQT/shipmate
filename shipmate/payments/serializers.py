b15e30abe57a2feaa491a7a7237b5a387feaaea8
STQT <productgsm@gmail.com>
2024-07-10 00:58:18 +0500

    fix

--- shipmate/payments/serializers.py
+++ shipmate/payments/serializers.py
@@ -46,26 +46,31 @@ class OrderPaymentSerializer(serializers.ModelSerializer):
 
 
 class OrderPaymentAttachmentSerializer(serializers.ModelSerializer):
-    image = Base64ImageField(use_url=True)
+    image = Base64ImageField(use_url=True, required=False)
     credit_card = serializers.IntegerField(allow_null=True, write_only=True, required=False)
 
     class Meta:
         model = OrderPaymentAttachment
         fields = ['order_payment', 'amount', 'image', 'payment_type', 'created_at', 'is_success', 'credit_card']
-
+        extra_kwargs = {
+            'is_success': {'read_only': True},
+            'payment_type': {'read_only': True},
+        }
     def create(self, validated_data):
         order_payment: OrderPayment = validated_data['order_payment']
         amount = validated_data['amount']
+        validated_data['payment_type'] = order_payment.payment_type
         credit_card = validated_data.pop("credit_card")
         if credit_card:
             credit_card: OrderPaymentCreditCard = OrderPaymentCreditCard.objects.get(pk=credit_card)
             result = charge_payment(amount, credit_card.card_number, credit_card.expiration_date, credit_card.cvv)
             if result['success'] is False:
+                validated_data['is_success'] = False
+                OrderPaymentAttachment.objects.create(**validated_data)
                 raise ValidationError({"credit_card": f"Problem via Authorize.net: {result['message']}"})
         order_payment_attachments_all_amount = OrderPaymentAttachment.objects.filter(order_payment=order_payment)
         total_amount = order_payment_attachments_all_amount.aggregate(Sum('amount'))['amount__sum'] or 0
         order_payment.amount_charged = total_amount + amount
-        validated_data['payment_type'] = order_payment.payment_type
         if order_payment.amount <= order_payment.amount_charged:
             order_payment.status = OrderPayment.StatusChoices.PAID
         order_payment.save()

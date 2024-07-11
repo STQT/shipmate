3154af16ca47d10cd7620c0dcfc1efd24958cb6b
STQT <productgsm@gmail.com>
2024-07-11 21:27:00 +0500

    steps filter

--- shipmate/more_settings/filters.py
+++ shipmate/more_settings/filters.py
@@ -5,7 +5,8 @@ from shipmate.more_settings.models import Automation
 
 class AutomationFilter(django_filters.FilterSet):
     status = django_filters.ChoiceFilter(choices=Automation.StatusChoices.choices)
+    steps = django_filters.ChoiceFilter(choices=Automation.StepsChoices.choices)
 
     class Meta:
         model = Automation
-        fields = ['status']
+        fields = ['status', 'steps']

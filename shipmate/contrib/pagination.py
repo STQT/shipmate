9f7a401798d856690c9fdc8a004e6701dc84c804
STQT <productgsm@gmail.com>
2024-07-16 23:15:14 +0500

    fix pagination

--- shipmate/contrib/pagination.py
+++ shipmate/contrib/pagination.py
@@ -4,3 +4,7 @@ from rest_framework.pagination import LimitOffsetPagination
 class CustomPagination(LimitOffsetPagination):
     default_limit = 10
     max_limit = 1000
+
+    def get_offset(self, request):
+        offset = super().get_offset(request)
+        return max(0, offset - 1)

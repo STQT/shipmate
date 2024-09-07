3921e56462472d3d90406b219ea71f6859d420df
Murodov Azizmurod <murodovazizmurod@gmail.com>
2024-09-07 14:58:06 +0500

    adding customer serializer

--- shipmate/customers/serializers.py
+++ shipmate/customers/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from django.db import models
 
 from .models import Customer, ExternalContacts
+from ..contrib.models import OrderStatusChoices
+from ..orders.models import Order
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -49,7 +52,43 @@ class SmallExternalContactsSerializer(serializers.Serializer):
 
 class DetailCustomerSerializer(serializers.ModelSerializer):
     extra = SmallExternalContactsSerializer(many=True)
+    complete = serializers.SerializerMethodField()
+    ongoing = serializers.SerializerMethodField()
+    uncompleted = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Customer
         fields = "__all__"
+
+    @classmethod
+    def get_complete(self, obj):
+        # Get all related orders and sum the reservation fee (payment_reservation)
+        orders = Order.objects.filter(customer_id=obj.id)
+        total_reservation_fee = orders.aggregate(total=models.Sum('payment_paid_reservation'))['total']
+        return total_reservation_fee or 0  # Return 0 if there are no orders
+
+
+    @classmethod
+    def get_ongoing(self, obj):
+        # Define the statuses for ongoing orders
+        ongoing_statuses = [
+            OrderStatusChoices.ORDERS,
+            OrderStatusChoices.BOOKED,
+            OrderStatusChoices.POSTED,
+            OrderStatusChoices.NOTSIGNED
+        ]
+        # Filter orders by user and ongoing statuses
+        orders = Order.objects.filter(customer_id=obj.id, status__in=ongoing_statuses)
+        total_ongoing_reservation = orders.aggregate(total=models.Sum('payment_reservation'))['total']
+        return total_ongoing_reservation or 0  # Return 0 if there are no ongoing orders
+
+
+
+    @classmethod
+    def get_uncompleted(self, obj):
+        # Filter orders by user and Archived status
+        orders = Order.objects.filter(customer_id=obj.id, status=OrderStatusChoices.ARCHIVED)
+        total_uncompleted_reservation = orders.aggregate(total=models.Sum('payment_reservation'))['total']
+        return total_uncompleted_reservation or 0  # Return 0 if there are no archived (uncompleted) orders
+

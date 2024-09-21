712014aefa90da1dc03731fcd2420c187dd699be
Murodov Azizmurod <murodovazizmurod@gmail.com>
2024-09-22 00:38:32 +0500

    phone error fix

--- shipmate/quotes/serializers.py
+++ shipmate/quotes/serializers.py
@@ -130,7 +130,7 @@ class QuoteAttachmentSerializer(serializers.ModelSerializer):
         if obj.type == Attachments.TypesChoices.PHONE:
             # Assuming `from_phone` is a field in the related order model
             phone = PhoneAttachment.objects.filter(id=obj.link).first()
-            return phone.from_phone
+            return phone.from_phone if phone else None
         else:
             return None
 

28119eeab0b54efd7120b800ad5303db7631478c
Murodov Azizmurod <murodovazizmurod@gmail.com>
2024-10-09 03:03:41 +0500

    trying insight

--- shipmate/insights/views.py
+++ shipmate/insights/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets  # noqa
+from django.contrib.auth import get_user_model
 
 from shipmate.contrib.generics import UpdatePUTAPIView
 from shipmate.insights.models import GoalGroup, LeadsInsight
@@ -8,6 +9,10 @@ from shipmate.insights.models import Goal
 from shipmate.insights.serializers import (
     ListGoalSerializer, RetrieveGoalSerializer, UpdateGoalSerializer, CreateGoalSerializer,
 )
+from shipmate.lead_managements.models import Provider
+
+
+User = get_user_model()
 
 
 class GoalCreateAPIView(generics.CreateAPIView):  # noqa
@@ -44,8 +49,37 @@ class GoalGroupViewSet(viewsets.ModelViewSet):
 
 # Lead Insight
 
-class LeadsInsightUsersAPIView(generics.ListAPIView):
+from rest_framework.response import Response
+from django.db.models import Count
+
+
+class LeadsInsightUsersAPIView(generics.GenericAPIView):
     queryset = LeadsInsight.objects.all()
-    serializer_class = ListLeadsInsightUserSerializer
-    filterset_class = LeadsInsightFilter
-    pagination_class = None
+
+    def get(self, request, *args, **kwargs):
+        # Get all users
+        users = User.objects.all()
+        user_labels = [f"{user.first_name} {user.last_name}" for user in users]
+
+        # Get all providers
+        providers = Provider.objects.all()
+
+        datasets = []
+
+        for provider in providers:
+            # Count LeadsInsight for each user associated with the current provider
+            leads_counts = [
+                LeadsInsight.objects.filter(user=user, source=provider).count()
+                for user in users
+            ]
+            # Add the data for the current provider to the dataset
+            datasets.append({
+                'label': provider.name,
+                'data': leads_counts,
+            })
+
+        # Return data in the required format
+        return Response({
+            'labels': user_labels,
+            'datasets': datasets,
+        })
